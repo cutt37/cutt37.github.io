61528eb1db882ab0c70adc8f257baf0fde8fda35
cutt37 <99665719+cutt37@users.noreply.github.com>
2022-09-01 20:50:23 +0800

    Create script.js

---
+++ scripts/script.js
@@ -0,0 +1,6 @@
+document.getElementById("head").innerHTML = "cutt37的网站";
+document.getElementById("footer").innerHTML = "ver_0.0.2b";
+document.getElementById("version").innerHTML = "ver_0.0.2b";
+document.getElementById("a_passage").innerHTML = "这是首页";
+document.getElementById("upd_text").innerHTML = "增加按钮动画效果";
+document.getElementById("upd_text_1").innerHTML = "";
