088b649e071e9d829c1aeafd5e744bb6a8bbcdc6
cutt37 <99665719+cutt37@users.noreply.github.com>
2022-09-05 16:38:54 +0800

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,3 @@
 document.getElementById("head").innerHTML = "cutt37的网站";
-document.getElementById("footer").innerHTML = "ver_0.0.2b";
+document.getElementById("footer").innerHTML = "ver_0.0.2c";
 document.getElementById("a_passage").innerHTML = "这是首页";
