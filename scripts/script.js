fd97723393d55eee50ed674e511ea0312248b925
cutt37 <99665719+cutt37@users.noreply.github.com>
2022-09-23 21:14:59 +0800

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -2,8 +2,16 @@ document.getElementById("head").innerHTML = "cutt37的网站";
 document.getElementById("footer").innerHTML = "ver_0.0.2c";
 document.getElementById("a_passage").innerHTML = "这是首页";
 
+function act_normal_home(obj) {
+    obj.innerHTML="首页"
+}
+
 function act_normal_repo(obj) {
     obj.innerHTML="仓库"
+}
+
+function act_normal_update(obj) {
+    obj.innerHTML="版本"
 }
 
 function act_hover(obj) {
