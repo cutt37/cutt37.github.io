44c2c7c830b963ac3f20bf34e591173bd64710b1
cutt37 <99665719+cutt37@users.noreply.github.com>
2022-09-23 20:41:14 +0800

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,11 @@
 document.getElementById("head").innerHTML = "cutt37的网站";
 document.getElementById("footer").innerHTML = "ver_0.0.2c";
 document.getElementById("a_passage").innerHTML = "这是首页";
+
+function act_normal_repo(obj) {
+    obj.innerHTML="仓库"
+}
+
+function act_hover(obj) {
+    obj.innerHTML="访问"
+}
